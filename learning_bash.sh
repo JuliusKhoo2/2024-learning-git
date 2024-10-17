c7bd84a9d8f089975a37a1d4d1ca52f8dc815083
Julius Khoo <jkhoo@dons.usfca.edu>
2024-10-16 17:55:09 -0700

    Create bash script to run wc on files

---
+++ learning_bash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# A script to learn about bash scripts and passing in paramaters
+# from the command line
+
+# Julius Khoo
+# October 16, 2024
+# jkhoo@dons.usfca.edu
+
+if [ $# -eq 0 ]
+then 
+	echo "Please supply a single filename as an argument."
+	exit 1
+fi
+
+
+for file in $@
+do
+	echo "Counting lines, words, and characters in $file..."
+	wc $file
+done
+
